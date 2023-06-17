36afca8f63836715ac7bc2d4afe4aee93d9e55e5
Lylay-09 <pozzlay549@gmail.com>
2023-06-17 15:16:35 +0700

    resize team member cover

--- utils/checkSocialMediaUrl.js
+++ utils/checkSocialMediaUrl.js
@@ -1,11 +1,13 @@
 export function checkSocialMediaURL(url) {
   if (url.includes("https://www.")) {
     return url;
-  } else if (url.includes("www.")) {
-    let combine = `https://${url}`;
-    return combine;
-  } else {
-    let combine = `https://www.${url}`;
+  }
+  // else if (url.includes("www.")) {
+  //   let combine = `https://${url}`;
+  //   return combine;
+  // }
+  else {
+    let combine = `${url}`;
     return combine;
   }
 }
